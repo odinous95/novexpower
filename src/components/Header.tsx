18d660a1a9ce4045dbe873099b1cfba2fbb74f47
Odin Thif <mjd.thif@icloud.com>
2025-03-26 23:11:54 +0100

    removed icon(old logo)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,6 @@ import Link from 'next/link';
 import React, { useState, useEffect } from 'react';
 import { Transition } from '@headlessui/react';
 import { HiOutlineXMark, HiBars3 } from 'react-icons/hi2';
-import { FaFingerprint } from 'react-icons/fa';
 import Container from './Container';
 import { siteDetails } from '@/data/siteDetails';
 import { menuItems } from '@/data/menuItems';
