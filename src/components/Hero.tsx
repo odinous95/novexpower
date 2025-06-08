bb156b8df2b175169078e561407858f77ac3a91a
Odin Thif <mjd.thif@icloud.com>
2025-06-08 22:21:52 +0200

    changed hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,7 +9,7 @@ const Hero: React.FC = () => {
             className="relative flex items-center justify-center pb-0 pt-32 md:pt-40 px-5"
         >
             <div className="absolute left-0 top-0 bottom-0 -z-10 w-1/2 h-full">
-                <div className="absolute inset-0 h-full w-full bg-[linear-gradient(to_left,#87ceeb12_1px,transparent_1px),linear-gradient(to_bottom,#87ceeb_1px,transparent_1px)] bg-[size:40px_30px] [mask-image:radial-gradient(ellipse_70%_100%_at_50%_100%,#000_60%,transparent_100%)]">
+                <div className="absolute inset-0 h-full w-full bg-[linear-gradient(to_left,#87ceeb12_1px,transparent_1px),linear-gradient(to_bottom,#87ceeb_1px,transparent_1px)] bg-[size:40px_30px] [mask-image:radial-gradient(ellipse_70%_100%_at_10%_100%,#000_10%,transparent_100%)]">
                 </div>
             </div>
             <div className="absolute right-0 top-0 -z-20 w-7/12 h-full">
