64a640fc6517ae87b6be41e2cf4fcedd0dee87a6
Odin Thif <mjd.thif@icloud.com>
2025-11-15 15:20:34 +0100

    #14 feat: add events data for upcoming events in events.ts

---
+++ frontend/src/data/events.ts
@@ -0,0 +1,15 @@
+
+export const events = [
+    {
+        name: "The Swedish Battery Reboot",
+        date: "25.11.25",
+        location: "Tekniska högskolan, Stockholm",
+        image: "/images/battery-reboot-1.jpeg"
+    },
+    {
+        name: "Exhibitors, Speakers & Contributors",
+        date: "25.11.25",
+        location: "Stockholm, Sweden",
+        image: "/images/battery-reboot-2.jpeg"
+    }
+];
