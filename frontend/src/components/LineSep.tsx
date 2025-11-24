0596522a2f6b56e156886362f1b80c3f9560c446
Odin Thif <mjd.thif@icloud.com>
2025-11-24 14:47:39 +0100

    fix: correct height class for LineSep component for consistent styling

--- frontend/src/components/LineSep.tsx
+++ frontend/src/components/LineSep.tsx
@@ -2,6 +2,6 @@ import React from "react";
 
 export function LineSep() {
     return (
-        <div className="mt-12 h-1 w-full bg-gradient-to-r from-transparent via-gray-300 to-transparent opacity-40 dark:via-gray-300" />
+        <div className="mt-12 h-px w-full bg-gradient-to-r from-transparent via-gray-300 to-transparent opacity-40 dark:via-gray-300" />
     )
 }
